e1ab982c3d182b03eb70a6f97eff94b72249287c
Lucian Green <luciangreen@lucianacademy.com>
2022-06-24 19:48:34 +1000

    Update time_machine.pl

--- time_machine.pl
+++ time_machine.pl
@@ -1,6 +1,7 @@
 % needs t2b repository
 
 :-include('../listprologinterpreter/listprolog.pl').
+:-include('../Text-to-Breasonings/text_to_breasonings.pl').
 
 time_machine_prepare :-
 
@@ -17,13 +18,13 @@ K=["../Lucian-Academy/Time Travel/",
 findall(J,(member(K1,K),	directory_files(K1,F),
 	delete_invisibles_etc(F,G),
 
-findall([File_term,"\n"],(member(H,G),open_file_s(H,File_term)),J)),J3),
+findall([File_term,"\n"],(member(H,G),string_concat(K1,H,H1),open_file_s(H1,File_term)),J)),J3),
 
 flatten(J3,J1),
 foldr(string_concat,J1,"",J2),
 
 
-N=1,M=u,texttobr2(N,u,J2,M,false,false,false,false,false,false).
+N=1,M=u,texttobr2(N,u,J2,M,false,false,false,false,false,false),
 
 
 N=1,M=u,texttobr(N,u,J2,M).
