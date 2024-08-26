c4cb6cc6f062bca5e02963788abec5bff99aad72
Lucian Green <luciangreen@lucianacademy.com>
2024-08-27 09:13:04 +1000

    Update t2b_br_adder.pl

--- t2b_br_adder.pl
+++ t2b_br_adder.pl
@@ -3,6 +3,9 @@
 
 t2b_br_adder(N) :-
 
+	get_time(TS2),
+	stamp_date_time(TS2,date(Y1, M1, D1, _, _, _, _, _, _),0),
+
 	working_directory(WD,WD),
 	%working_directory(_,'../../private/'),	
 	open_file_s("../Time_Machine/analogy_sentence_n.txt",[D2,M2,Y2,n=SN2]),
